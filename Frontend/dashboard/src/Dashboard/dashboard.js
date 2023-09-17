c7be8a94ba89a58404230895d483d088ce521322
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-09-18 01:09:59 +0530

    Time Calculated

--- Frontend/dashboard/src/Dashboard/dashboard.js
+++ Frontend/dashboard/src/Dashboard/dashboard.js
@@ -4,6 +4,8 @@ import './dashboard.css';
 function Dashboard(){
     const [userData, setUserData] = useState([]);
     const [totalWorkTime, setTotalWorkTime] = useState(0);
+    const [totalOverTime, setTotalOverTime] = useState(0);
+
     const fetchUserData = () => {
         fetch(`http://localhost:3000/users/dashboard?user_id=1`)
         .then(response => {
@@ -12,6 +14,7 @@ function Dashboard(){
         .then(data => {
             setUserData(data)
             calculateTotalWorkTime(data.data);
+            calculateTotalOverWorkTime(data.data);
         })
     }
   
@@ -28,9 +31,43 @@ function Dashboard(){
     }
 
     const calculateDayWorkTime = (dayData) => {
-        const startTime = new Date(dayData.start_time);
-        const endTime = new Date(dayData.end_time);
-        return (endTime - startTime)/60*60*1000;
+        const startTime = dayData.start_time;
+        const startTimeArray = startTime.split(':');
+        const startHours = startTimeArray[0] * 60 * 60;
+        const startMinutes =  startTimeArray[1] * 60;
+        const startSeconds = startTimeArray[2];
+        const  getStartTime = (startHours + startMinutes + startSeconds);
+
+        const endTime = dayData.end_time;
+        const endTimeArray = endTime.split(':');
+        const endHours = endTimeArray[0] * 60 * 60;
+        const endTMinutes = endTimeArray[1] * 60;
+        const endSeconds = endTimeArray[2];
+        const getEndTime = (endHours + endTMinutes + endSeconds);
+        console.log(getEndTime - getStartTime)
+        return (getEndTime - getStartTime)/(60*60*100);
+    }
+
+    const calculateTotalOverWorkTime = (data) => {
+        let totalOverTime = 0;
+        for(const dayData of data){
+            totalOverTime += calculateTotalOverTime(dayData);
+        }
+        setTotalOverTime(totalOverTime);
+    }
+
+    const calculateTotalOverTime = (overTimeData) => {
+        const endOverTime = overTimeData.end_time;
+        const endOverTimeArray = endOverTime.split(':');
+        const endOverHours = endOverTimeArray[0] * 60 * 60;
+        const endOverMinutes = endOverTimeArray[1] * 60;
+        const endOverSeconds = endOverTimeArray[2];
+        const getEndOverTime = (endOverHours + endOverMinutes + endOverSeconds);
+        if(getEndOverTime > 6120000){
+            return (getEndOverTime - 6120000)/(60*60*100);
+        } else {
+            return 0;
+        }
     }
     return(
         <div className="dashboard">
@@ -48,6 +85,7 @@ function Dashboard(){
 
                 <div>    
                 <label className="time">Overtime Hours</label>
+                <p>{totalOverTime}</p>
                 {/* <button className="clickTwo">click</button> */}
                 </div>
             </div>
